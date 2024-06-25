8aab371481174445eed28a393b2e3a72c7117740
Lin Khant Ko <lkk19@bu.edu>
2024-06-25 19:17:35 -0400

    fixed an issue where images were not showing correctly

--- src/App.js
+++ src/App.js
@@ -3,19 +3,32 @@ import './App.css';
 import Header from './components/Header';
 import GridItem from './components/GridItem';
 
+// importing from src images. 
+import img1 from './images/1.jpg';
+import img2 from './images/2.jpg';
+import img3 from './images/3.jpg';
+import img4 from './images/4.jpg';
+import img5 from './images/5.jpg';
+import img6 from './images/6.jpg';
+import img7 from './images/7.jpg';
+import img8 from './images/8.jpg';
+import img9 from './images/9.jpg';
+
 function App() {
   const items = [
     // my images are in images folder in public folder.
     // public can be directly accessed via root path!
-    { image: '/images/1.jpg', altText: 'Image 1', overlayText: 'Crown Dreams' },
-    { image: '/images/2.jpg', altText: 'Image 2' },
-    { image: '/images/3.jpg', altText: 'Image 3', overlayText: 'San Jose' },
-    { image: '/images/4.jpg', altText: 'Image 4', overlayText: 'Creative Writing Class' },
-    { image: '/images/5.jpg', altText: 'Image 5', overlayText: 'Seven Ways to Heaven' },
-    { image: '/images/6.jpg', altText: 'Image 6', overlayText: 'Imaginary' },
-    { image: '/images/7.jpg', altText: 'Image 7', overlayText: 'New People' },
-    { image: '/images/8.jpg', altText: 'Image 8', overlayText: 'Jane' },
-    { image: '/images/9.jpg', altText: 'Image 9' },
+    // Update: I changed images into src folder instead because public folder was causing bugs.
+
+    { image: img1, altText: 'Image 1', overlayText: 'Crown Dreams' },
+    { image: img2, altText: 'Image 2' },
+    { image: img3, altText: 'Image 3', overlayText: 'San Jose' },
+    { image: img4, altText: 'Image 4', overlayText: 'Creative Writing Class' },
+    { image: img5, altText: 'Image 5', overlayText: 'Seven Ways to Heaven' },
+    { image: img6, altText: 'Image 6', overlayText: 'Imaginary' },
+    { image: img7, altText: 'Image 7', overlayText: 'New People' },
+    { image: img8, altText: 'Image 8', overlayText: 'Jane' },
+    { image: img9, altText: 'Image 9' },
   ];
 
   // images will be replaced later when I actually have taken good photos. 
