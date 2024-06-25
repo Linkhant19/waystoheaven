a83ef6d525d8843f704e1c5552db1cd2f06b759b
Lin Khant Ko <lkk19@bu.edu>
2024-06-25 19:02:48 -0400

    changed background color

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
   const items = [
     // my images are in images folder in public folder.
     // public can be directly accessed via root path!
-    { image: 'images/1.jpg', altText: 'Image 1', overlayText: 'Crown Dreams' },
-    { image: 'images/2.jpg', altText: 'Image 2' },
-    { image: 'images/3.jpg', altText: 'Image 3', overlayText: 'San Jose' },
-    { image: 'images/4.jpg', altText: 'Image 4', overlayText: 'Creative Writing Class' },
-    { image: 'images/5.jpg', altText: 'Image 5', overlayText: 'Seven Ways to Heaven' },
-    { image: 'images/6.jpg', altText: 'Image 6', overlayText: 'Imaginary' },
-    { image: 'images/7.jpg', altText: 'Image 7', overlayText: 'New People' },
-    { image: 'images/8.jpg', altText: 'Image 8', overlayText: 'Jane' },
-    { image: 'images/9.jpg', altText: 'Image 9' },
+    { image: '/images/1.jpg', altText: 'Image 1', overlayText: 'Crown Dreams' },
+    { image: '/images/2.jpg', altText: 'Image 2' },
+    { image: '/images/3.jpg', altText: 'Image 3', overlayText: 'San Jose' },
+    { image: '/images/4.jpg', altText: 'Image 4', overlayText: 'Creative Writing Class' },
+    { image: '/images/5.jpg', altText: 'Image 5', overlayText: 'Seven Ways to Heaven' },
+    { image: '/images/6.jpg', altText: 'Image 6', overlayText: 'Imaginary' },
+    { image: '/images/7.jpg', altText: 'Image 7', overlayText: 'New People' },
+    { image: '/images/8.jpg', altText: 'Image 8', overlayText: 'Jane' },
+    { image: '/images/9.jpg', altText: 'Image 9' },
   ];
 
   // images will be replaced later when I actually have taken good photos. 
